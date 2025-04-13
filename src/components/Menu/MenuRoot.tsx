7b3ba5f0cf8c73c910a23d199ef2d9a8eaa5761c
Douglas Carvalho <dscarvalho199@gmail.com>
2025-04-13 17:46:55 -0300

    refactor: Remove unnecessary React import in MenuRoot component

--- src/components/Menu/MenuRoot.tsx
+++ src/components/Menu/MenuRoot.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import styles from "./styles.module.css";
 export default function MenuRoot(props: PropsWithChildren) {
   return <nav className={styles.menu}>{props.children}</nav>;
